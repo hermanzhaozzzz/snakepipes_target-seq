3502b9403fa8c4d44519e1081f2315150c6dea86
hermanzhaozzzz <hermanzhaozzzz@gmail.com>
2020-11-21 20:29:53 +0800

    update

---
+++ .ipynb_checkpoints/step.03.snk.quantify_for_every_region-checkpoint.py
@@ -0,0 +1,104 @@
+# _*_ coding: UTF-8 _*_
+
+########################################################################
+# ZHAO Huanan
+# 2020-05-04
+# fastqc + multiqc pipeline
+######################################################################## 
+# run on abyss
+
+# --------------------------------------------------------------->>>>>>>
+# pipeline
+# --------------------------------------------------------------->>>>>>>
+# 1. fastqc
+# 2. multiqc
+# --------------------------------------------------------------->>>>>>>
+# software
+# --------------------------------------------------------------->>>>>>>
+FASTQC = "/home/zhaohuanan/zhaohn_HD/miniconda3/bin/fastqc"
+MULTIQC = "/home/zhaohuanan/zhaohn_HD/miniconda3/bin/multiqc"
+
+
+
+
+
+# --------------------------------------------------------------->>>>>>>
+# vars
+# --------------------------------------------------------------->>>>>>>
+LIB = ['CBE-EMX1-rep1', 'CBE-EMX2-rep2']
+REGION = ['EMX1-Guideseq-1',
+ 'EMX1-Guideseq-10',
+ 'EMX1-Guideseq-11',
+ 'EMX1-Guideseq-12',
+ 'EMX1-Guideseq-13',
+ 'EMX1-Guideseq-14',
+ 'EMX1-Guideseq-15',
+ 'EMX1-Guideseq-2',
+ 'EMX1-Guideseq-3',
+ 'EMX1-Guideseq-4',
+ 'EMX1-Guideseq-5',
+ 'EMX1-Guideseq-6',
+ 'EMX1-Guideseq-7',
+ 'EMX1-Guideseq-8',
+ 'EMX1-Guideseq-9']
+
+
+
+# ------------------------------------------------------------------------------------------>>>>>>>>>>
+# rule all
+# ------------------------------------------------------------------------------------------>>>>>>>>>>
+rule all:
+    # 有点特殊，输出和输入文件名只能有R1.fastq.gz和_fastqc.html的区别，前面不能有区别！要改就得全改！
+    input:
+        expand("../TargetSeq-{lib}/demultiplex.fastq/{region}_demultiplex_R1.fastq",lib=LIB, region=REGION),
+        expand("../TargetSeq-{lib}/demultiplex.fastq/{region}_demultiplex_R2.fastq",lib=LIB, region=REGION),
+        expand("../qc_quantify-all/fastqc/TargetSeq-{lib}_{region}",lib=LIB, region=REGION),
+        "../qc_quantify-all/multiqc/multiqc_report.html"
+# ------------------------------------------------------------------------------------------>>>>>>>>>>
+# rule fastqc
+# ------------------------------------------------------------------------------------------>>>>>>>>>>
+rule cp:
+    input: 
+        "../TargetSeq-{lib}/demultiplex.fastq/{region}_demultiplex_R1.fastq",
+        "../TargetSeq-{lib}/demultiplex.fastq/{region}_demultiplex_R2.fastq"
+    output: 
+        temp("../qc_quantify-all/fastq/TargetSeq-{lib}_{region}_demultiplex_R1.fastq"),
+        temp("../qc_quantify-all/fastq/TargetSeq-{lib}_{region}_demultiplex_R2.fastq"),
+    shell:
+        """
+        cp {input[0]} {output[0]}
+        cp {input[1]} {output[1]}
+        """
+rule fastqc: 
+    input: 
+        "../qc_quantify-all/fastq/TargetSeq-{lib}_{region}_demultiplex_R1.fastq",
+        "../qc_quantify-all/fastq/TargetSeq-{lib}_{region}_demultiplex_R2.fastq",
+    output: 
+        directory("../qc_quantify-all/fastqc/TargetSeq-{lib}_{region}")
+                # the suffix _fastqc.zip is necessary the same with SAMPLES raw name suffix, for multiqc to find the file. 
+                # If not using multiqc, you are free to choose an arbitrary filename
+    params: 
+        threads = "24",
+    log:
+        "../qc_quantify-all/fastqc/TargetSeq-{lib}/{region}.log"
+    shell:
+        """
+        mkdir -p {output}
+        {FASTQC} -o {output} -t {params.threads} \
+        {input[0]} {input[1]} 2>{log}
+        """
+# ------------------------------------------------------------------------------------------>>>>>>>>>>
+# rule multiqc
+# ------------------------------------------------------------------------------------------>>>>>>>>>>
+rule multiqc: 
+    input: 
+        expand("../qc_quantify-all/fastqc/TargetSeq-{lib}_{region}",lib=LIB, region=REGION)
+                # the suffix XXX_fastqc.zip is necessary the same with SAMPLES raw name suffix, for multiqc to find the file. 
+    output:
+        "../qc_quantify-all/multiqc/multiqc_report.html"
+    log:
+        "../qc_quantify-all/multiqc.log"
+    shell: 
+        """
+        {MULTIQC} ../qc_quantify-all/fastqc -o ../qc_quantify-all/multiqc --no-data-dir 2>{log}
+        """
---
+++ step.04.snk.merge_fastq_pipline.py
@@ -0,0 +1,48 @@
+# _*_ coding: UTF-8 _*_
+
+########################################################################
+# Herman ZHAO
+# 2020-07-27
+# run demultiplex target seq [ APO Mut site ]
+########################################################################
+# run on abyss
+PYTHON = "/home/zhaohuanan/zhaohn_HD/miniconda3/envs/snakepipes_target-seq/bin/python"
+
+
+
+
+PRIMER_INFO = "./primer_table/sub6.txt"
+
+
+
+CUTOFF = ["3","5","10"]
+
+LIBS = ['CBE-EMX1-rep1', 'CBE-EMX2-rep2']
+
+
+rule all:
+    input:
+        expand("../TargetSeq-{lib}/cutoff_{cutoff}/{lib}_cutoff_{cutoff}.report",lib=LIBS,cutoff=CUTOFF)
+
+rule MergeFastq:
+    input:
+        "../TargetSeq-{lib}"
+    output:
+        "../TargetSeq-{lib}/cutoff_{cutoff}/{lib}_cutoff_{cutoff}.report"
+    log:
+        "../TargetSeq-{lib}/cutoff_{cutoff}/{lib}_cutoff_{cutoff}.log"
+    params:
+        out_dir = "../TargetSeq-{lib}/cutoff_{cutoff}",
+        cutoff = "{cutoff}"
+    shell:
+#         """
+#         srun -T 24 \
+        """
+        {PYTHON} ./program/target_seq_merge_fq_V03.py \
+        -i {input[0]} \
+        -p {PRIMER_INFO} \
+        -o {params.out_dir} \
+        -r {output} \
+        --MinMergeReadNumCutoff {params.cutoff}  > {log} 2>&1
+        """
+
